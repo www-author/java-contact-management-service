c9ed5d41f2e5d38a05d72cbb79ef5a875612b258
www-author <dream970215@naver.com>
2024-02-08 16:59:36 +0900

    :sparkles: feat : Application 메인 함수 추가

---
+++ src/main/java/contact/management/Application.java
@@ -0,0 +1,15 @@
+package main.java.contact.management;
+
+public class Application {
+    public static void main(String[] args) {
+
+/*        """
+        1. 비즈니스 연락처 추가
+        2. 개인 연락처 추가
+        3. 연락처 출력
+        4. 연락처 검색
+        5. 종료
+        메뉴를 선택해주세요: ↵"""*/
+
+    }
+}
