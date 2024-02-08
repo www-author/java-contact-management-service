41487e9b62f54aaf69d5b50f77d9bf424a5893a1
www-author <dream970215@naver.com>
2024-02-08 17:00:07 +0900

    :sparkles: feat : 주소록 클래스 생성

---
+++ src/main/java/contact/management/domain/address/AddressBook.java
@@ -0,0 +1,17 @@
+package main.java.contact.management.domain.address;
+
+import java.util.List;
+import main.java.contact.management.domain.contact.Contact;
+
+public class AddressBook {
+    private List<Contact> contacts;
+
+    public void displayContacts() {
+
+    }
+
+    public void searchContact(String name) {
+
+    }
+
+}
