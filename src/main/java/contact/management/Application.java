2a215f21e80f591fbc854f7629814036b6654270
www-author <dream970215@naver.com>
2024-02-12 01:35:08 +0900

    :sparkles: feat : 메인 컨트롤러 호출

--- src/main/java/contact/management/Application.java
+++ src/main/java/contact/management/Application.java
@@ -1,15 +1,14 @@
 package main.java.contact.management;
 
+import main.java.contact.management.controller.ContactManagementController;
+
 public class Application {
     public static void main(String[] args) {
-
-/*        """
-        1. 비즈니스 연락처 추가
-        2. 개인 연락처 추가
-        3. 연락처 출력
-        4. 연락처 검색
-        5. 종료
-        메뉴를 선택해주세요: ↵"""*/
-
+        ContactManagementController contactManagementController = new ContactManagementController();
+        contactManagementController.run();
     }
 }
+
+/* 아래 같은 방법도 있어욤! */
+// 반복문 대신에
+// 이벤트 리스너 방식 ? => 각 조건에 이벤트 리스너를 달아서 하기
