694025a16c075e0b9443fc8a7f45283020df3bd5
www-author <dream970215@naver.com>
2024-02-12 01:23:59 +0900

    :sparkles: feat : 정적 팩토리 패턴 적용 및 개별 toString 메서드 추가

--- src/main/java/contact/management/domain/contact/BusinessContact.java
+++ src/main/java/contact/management/domain/contact/BusinessContact.java
@@ -1,14 +1,31 @@
 package main.java.contact.management.domain.contact;
 
 public class BusinessContact extends Contact {
-    private String company;
+    private final String company;
 
-    public BusinessContact(
+    private BusinessContact(
             String name,
             String phoneNumber,
             String company
     ) {
         super(name, phoneNumber);
         this.company = company;
+    }
+
+    public static BusinessContact of(
+            String name,
+            String phoneNumber,
+            String company
+    ) {
+        return new BusinessContact(name, phoneNumber, company);
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    @Override
+    public String toString() {
+        return "이름: " + getName() + ", 전화번호: " + getPhoneNumber() + ", 회사명: " + getCompany();
     }
 }
--- src/main/java/contact/management/domain/contact/PersonalContact.java
+++ src/main/java/contact/management/domain/contact/PersonalContact.java
@@ -1,14 +1,31 @@
 package main.java.contact.management.domain.contact;
 
 public class PersonalContact extends Contact  {
-    private String relationship;
+    private final String relationship;
 
-    public PersonalContact(
+    private PersonalContact(
             String name,
             String phoneNumber,
             String relationship
     ) {
         super(name, phoneNumber);
         this.relationship = relationship;
+    }
+
+    public static PersonalContact of(
+            String name,
+            String phoneNumber,
+            String relationship
+    ) {
+        return new PersonalContact(name, phoneNumber, relationship);
+    }
+
+    public String getRelationship() {
+        return relationship;
+    }
+
+    @Override
+    public String toString() {
+        return "이름: " + getName() + ", 전화번호: " + getPhoneNumber() + ", 관계: " + getRelationship();
     }
 }
