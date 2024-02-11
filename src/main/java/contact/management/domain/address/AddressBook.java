01e20b8d04e62472a423cce5c4a8c8e24d3f6cc8
www-author <dream970215@naver.com>
2024-02-12 01:34:30 +0900

    :sparkles: feat : 주소록 정적 팩토리 메서드 적용 및 메서드 추가

--- src/main/java/contact/management/domain/address/AddressBook.java
+++ src/main/java/contact/management/domain/address/AddressBook.java
@@ -1,17 +1,48 @@
 package main.java.contact.management.domain.address;
 
-import java.util.List;
+import main.java.contact.management.constants.ExceptionMessage;
 import main.java.contact.management.domain.contact.Contact;
+
+import java.util.List;
 
 public class AddressBook {
     private List<Contact> contacts;
 
-    public void displayContacts() {
-
+    private AddressBook(List<Contact> contacts) {
+        this.contacts = contacts;
     }
 
-    public void searchContact(String name) {
-
+    public static AddressBook from(List<Contact> contacts) {
+        return new AddressBook(contacts);
     }
 
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void addContacts(List<Contact> contacts) {
+        this.contacts.addAll(contacts);
+    }
+
+    public String displayContacts(List<Contact> contacts) {
+        StringBuilder message = new StringBuilder();
+        for (Contact contact : contacts) {
+            message.append(contact).append("\n");
+        }
+        return String.valueOf(message);
+    }
+
+    public String searchContact(String name) {
+        StringBuilder message = new StringBuilder();
+        for (Contact contact : contacts) {
+            if(name.equals(contact.getName())) {
+                message.append("\n").append(contact);
+            }
+        }
+
+        if(name.isEmpty() || message.isEmpty()) {
+            return ExceptionMessage.NOT_FOUND_CONTACT.getMessage();
+        }
+        return String.valueOf(message);
+    }
 }
